8ffde0bf69338a3f40c2addde2e3f362acc56703
actics <alexander.lavrukov@gmail.com>
2012-12-01 19:27:36 +0600

    Задача 6: Сравнение строк

---
+++ strio/string.asm
@@ -0,0 +1,37 @@
+section .text
+	global stringcmp
+
+; int __cdecl stringcmp(char *str1, char *str2)
+stringcmp:
+	push rbp
+	mov rbp, rsp
+	
+	mov rsi, [rbp + 8*2]
+	mov rdi, [rbp + 8*3]
+
+	xor rax, rax
+
+	cld
+.while:
+	cmpsb
+	jl .less
+	jg .great
+
+	cmp byte [rsi], 0
+	je .exit
+
+	jmp .while
+
+.less:
+	mov rax, -1
+	jmp .exit
+
+.great:
+	mov rax, 1
+	jmp .exit
+
+.exit:
+	mov rsp, rbp
+	pop rbp
+	ret
+
